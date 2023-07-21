6012303c72b0417b6f43d3dbeee03abfe8733a85
Petra Andersson <petra.andersson@kalmarglobal.com>
2023-07-21 10:57:53 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState, useRef } from "react";
+import jogga from './jogga.mp4'
 
 function App() {
+  
+  const [mystream, setmystream] = useState(null);
+  const [videoswitch, setvideo] = useState(false);
+  const [isHidden, setHidden] = useState(true);
+  const windowSize = useRef([window.innerWidth, window.innerHeight]);
+
+  const videoRef = useRef();
+  const handleClick = () => {
+    setvideo(true)
+    setHidden(false)
+    videoRef.current.play()
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header"> 
+      <div>
+    <h3 className={isHidden ? 'App-logo': 'headertext'}>
+      JONKKAJONKKAJONKKA</h3>
+    { !videoswitch &&
+      <button className="jogga-button"
+      onClick={handleClick}
+      >
+        Click here to JONKKAJONKKAJONKKA
+      </button>
+    
+      }
+    <video 
+      ref={videoRef} 
+      hidden={isHidden}
+      width={windowSize.current[0]}
+      height={windowSize.current[1]*0.9}
+    >
+       <source src={jogga} type="video/mp4" />
+    </video>
     </div>
+      </header>
+      </div>
   );
 }
 
 export default App;
+
